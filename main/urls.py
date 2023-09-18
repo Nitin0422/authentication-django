02eca50878e8030c0f9f064fa3399b87c348d019
Nitin Tandukar <nitintandukar@Nitins-MacBook-Air.local>
2023-09-18 11:16:58 +0545

    Added link to login page

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,6 @@ app_name = "main"
 
 urlpatterns = [
     path("", views.HomePage, name="home"),
-    path("register/", views.Register, name="register")
+    path("register/", views.Register, name="register"),
+    path("login/", views.Login, name="login")
 ]
