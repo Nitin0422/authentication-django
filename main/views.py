f4ce9b021a2ae1f3b5055665907741548eb825aa
Nitin Tandukar <nitintandukar@Nitins-MacBook-Air.local>
2023-09-18 10:36:05 +0545

    Registration logic  added.

--- main/views.py
+++ main/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib import messages
+from .forms import RegistrationForm
 
 # Create your views here.
+def Register(request):
+    #If the request is a post method (Form submission)
+    if request.method == "POST":
+        #Extracting the submitted form
+        form = RegistrationForm(request.POST)
+        #If the form is valid
+        if form.is_valid():
+            #Calling the save method that will return a user instance with details binded to the user instance
+            user = form.save()
+            #calling inbuilt method for login authentication.
+            login(request, user)
+            #Setting up a message to show in the redirected page.
+            messages.success(request, "Successfully registered.")
+            return redirect("main:home")
+        else:
+            messages.error(request, "Invalid information. Try again!")
+            return redirect("main:home")
+    # If the request is a get request, new form is rendered.
+    form = RegistrationForm()
+    return render(request, "main/register.html", {"form": form})
