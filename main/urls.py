c12eca3d98c825d9f8c63397c97a37fee61af481
Nitin Tandukar <nitintandukar@Nitins-MacBook-Air.local>
2023-09-18 10:37:31 +0545

    URLS configured for registration

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views
+
+
+app_name = "main"
 
 urlpatterns = [
-    
+    path("", views.HomePage, name="home"),
+    path("register/", views.Register, name="register")
 ]
