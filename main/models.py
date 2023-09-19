2166b316d394e4160b39d1b23fbd9b14f4b4ef32
Nitin Tandukar <nitintandukar@Nitins-MacBook-Air.local>
2023-09-19 10:50:50 +0545

    Created two new models classroom and student

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class ClassRoom(models.Model):
+    class_name = models.CharField(max_length=200)
+    block = models.CharField(max_length=200)
+
+class Student(models.Model):
+    classroom = models.ForeignKey(ClassRoom, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+
